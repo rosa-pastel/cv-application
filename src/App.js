5166f5f6c68dbcf0fdb642c92e1eae21464c44f2
rosa-pastel <zeynepranamutlu@outlook.com>
2023-05-08 08:39:39 +0300

    Add pdf downloading feature

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Experience from "./components/experience";
 import General from "./components/general";
 import Skills from "./components/skills";
 import "./styles/app.css";
+import * as html2canvas from "html2canvas";
+import { jsPDF } from "jspdf";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { mode: "saved" };
     this.toggleMode = this.toggleMode.bind(this);
     this.handleEditBtnClick = this.handleEditBtnClick.bind(this);
+    this.printDocument = this.printDocument.bind(this);
   }
   handleEditBtnClick(e) {
     this.toggleEditBtn(e.target);
@@ -25,6 +28,16 @@ export default class App extends React.Component {
     btn.textContent = btn.textContent === "SAVE" ? "EDIT" : "SAVE";
     btn.className = btn.className === "save" ? "edit" : "save";
   }
+  printDocument() {
+    const input = document.getElementById("cv");
+    html2canvas(input).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF();
+      pdf.addImage(imgData, "JPEG", 0, 0);
+      pdf.output("dataurlnewwindow");
+      pdf.save("download.pdf");
+    });
+  }
   render() {
     return (
       <div>
@@ -35,10 +48,15 @@ export default class App extends React.Component {
         >
           EDIT
         </button>
-        <General mode={this.state.mode}></General>
-        <Education mode={this.state.mode}></Education>
-        <Experience mode={this.state.mode}></Experience>
-        <Skills mode={this.state.mode}></Skills>
+        <button id="download" onClick={this.printDocument}>
+          Download PDF
+        </button>
+        <div id="cv">
+          <General mode={this.state.mode}></General>
+          <Education mode={this.state.mode}></Education>
+          <Experience mode={this.state.mode}></Experience>
+          <Skills mode={this.state.mode}></Skills>
+        </div>
       </div>
     );
   }
